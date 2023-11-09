723596eac7f3aa4bae9e1b4aaf743b7ea9c8be10
Eita Okutani <hw22a044@oecu.jp>
2023-11-09 14:43:30 +0900

    for文を追加

--- mac_drawpad-main/DrawPad/main.cpp
+++ mac_drawpad-main/DrawPad/main.cpp
@@ -21,6 +21,9 @@ void DrawMain()
     }
     for (int x = 50; x < 250; x+=10) {
         Paint(x+5, 0, kColorDarkBlue, kColorBlue);
+        
+        for (int x = 50; x < 250; x+=10) {
+            Paint(x+5, 0, kColorDarkBlue, kColorBlue);
     }
 
     DrawText("Hello, world!!", -12*14/2, 44, kColorWhite);
@@ -34,5 +37,5 @@ void DrawMain()
     DrawText("Hello, world!!", -12*14/2, -44, kColorRed);
     Sleep(0.5f);
     DrawText("Hello, world!!", -12*14/2, -66, kColorGray);
-}
+    }
 
